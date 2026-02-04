b59f507631ada6420dd106f111491a669b0e80f6
ryancho-0 <ryan@biyard.co>
2026-02-04 17:10:12 +0900

    feat(landing): add icons

---
+++ landing/src/assets/index.tsx
@@ -0,0 +1,21 @@
+import { default as Dashboard3 } from './icons/dashboard-3.svg?react';
+import { default as Flag } from './icons/flag.svg?react';
+import { default as FlightTakeoff } from './icons/flight-takeoff.svg?react';
+import { default as LaptopSettings } from './icons/laptop-settings.svg?react';
+import { default as MyLocation } from './icons/my-location.svg?react';
+import { default as ShieldGood } from './icons/shield-good.svg?react';
+
+import { default as TrendingUp } from './icons/trending-up.svg?react';
+import { default as Wallet } from './icons/wallet.svg?react';
+
+
+export {
+    Dashboard3,
+    Flag,
+    FlightTakeoff,
+    LaptopSettings,
+    MyLocation,
+    ShieldGood,
+    TrendingUp,
+    Wallet
+}
