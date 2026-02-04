86c2633d221b0da90bea192d951132ee83298ce7
ryancho-0 <ryan@biyard.co>
2026-02-04 17:39:22 +0900

    chore(landing): move index file

---
+++ landing/src/assets/icons/index.tsx
@@ -0,0 +1,20 @@
+import { default as Dashboard3 } from './dashboard-3.svg?react';
+import { default as Flag } from './flag.svg?react';
+import { default as FlightTakeoff } from './flight-takeoff.svg?react';
+import { default as LaptopSettings } from './laptop-settings.svg?react';
+import { default as MyLocation } from './my-location.svg?react';
+import { default as ShieldGood } from './shield-good.svg?react';
+import { default as TrendingUp } from './trending-up.svg?react';
+import { default as Wallet } from './wallet.svg?react';
+
+
+export {
+    Dashboard3,
+    Flag,
+    FlightTakeoff,
+    LaptopSettings,
+    MyLocation,
+    ShieldGood,
+    TrendingUp,
+    Wallet
+}
