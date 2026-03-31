712de19a173bd58127140dce7e190cc153969ce3
ryancho-0 <ryan@biyard.co>
2026-03-31 15:48:43 +0900

    revert

--- landing/src/router.tsx
+++ landing/src/router.tsx
@@ -1,8 +1,5 @@
 import { createBrowserRouter } from "react-router";
 import { IndexPage } from "./pages";
-import { SignInPage } from "./pages/signin";
-import { WalletPage } from "./pages/wallet";
-import { DAOPage } from "./pages/dao";
 
 export const routes = createBrowserRouter(
   [
@@ -12,24 +9,8 @@ export const routes = createBrowserRouter(
       index: true,
       Component: IndexPage,
     },
-    {
-      path: "/signin",
-      Component: SignInPage,
-    },
-    {
-      path: "/signup",
-      Component: SignInPage,
-    },
-    {
-      path: "/wallet",
-      Component: WalletPage,
-    },
-    {
-      path: "/dao",
-      Component: DAOPage,
-    },
   ],
   {
     basename: import.meta.env.BASE_URL,
-  }
+  },
 );
