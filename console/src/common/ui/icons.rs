f117b475a1bbaa6dfa535e9400c71438898e4840
ryancho-0 <ryan@biyard.co>
2026-04-15 19:55:16 +0900

    feat(console): add docs side menu

--- console/src/common/ui/icons.rs
+++ console/src/common/ui/icons.rs
@@ -524,6 +524,24 @@ pub fn IconUpload(#[props(default = DEFAULT_CLASS)] class: &'static str) -> Elem
 }
 
 #[component]
+pub fn IconBookOpen(#[props(default = DEFAULT_CLASS)] class: &'static str) -> Element {
+    rsx! {
+        svg {
+            class,
+            xmlns: "http://www.w3.org/2000/svg",
+            view_box: "0 0 24 24",
+            fill: "none",
+            stroke: "currentColor",
+            stroke_width: "1.5",
+            stroke_linecap: "round",
+            stroke_linejoin: "round",
+            path { d: "M2 3h6a4 4 0 014 4v14a3 3 0 00-3-3H2z" }
+            path { d: "M22 3h-6a4 4 0 00-4 4v14a3 3 0 013-3h7z" }
+        }
+    }
+}
+
+#[component]
 pub fn IconBuilding(#[props(default = DEFAULT_CLASS)] class: &'static str) -> Element {
     rsx! {
         svg {
